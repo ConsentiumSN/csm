c73e561187a10173255edf32e4565b328c208a12
Shivam Bohare <shivambohare@gmail.com>
2020-03-27 10:40:37 +0530

    changes to csm script

--- scripts/csm.sh
+++ scripts/csm.sh
@@ -1,3 +1,3 @@
-find ./ -type f -readable  -writable -exec sed -i 's/public_key_legacy_prefix = "EOS"/public_key_legacy_prefix = "CSM"/g' {} \;
-find ./ -type f -readable  -writable -exec sed -i 's/EOSIO_ROOT_KEY "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV/EOSIO_ROOT_KEY "CSM6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV/g' {} \;
+find ./ -type f -name "public_key.hpp" -readable  -writable -exec sed -i 's/public_key_legacy_prefix = "EOS"/public_key_legacy_prefix = "CSM"/g' {} \;
+find ./ -type f -name "CMakeLists.txt" -readable  -writable -exec sed -i 's/EOSIO_ROOT_KEY "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV/EOSIO_ROOT_KEY "CSM6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV/g' {} \;
 echo "done"
